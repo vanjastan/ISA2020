d89b3efd1f12f1b8724b101aaeddd025e7628253
Jelena <stojanovicj97@gmail.com>
2021-02-03 15:48:51 +0100

    Fixed bug in getting all prescriptions

--- frontend/src/app/services/prescriptions.service.ts
+++ frontend/src/app/services/prescriptions.service.ts
@@ -12,12 +12,8 @@ import { USER_ID_KEY } from '../config/local-storage-keys';
     constructor(private http: HttpClient) { 
     }
 
-    getPrescriptions(id:number):Observable<Prescription[]>{
-        return this.http.get<Prescription[]>(`http://localhost:8080/prescriptions/forPatient/` + id);
-    }
-
-    getPatient(): Observable<any> {
+    getPrescriptions():Observable<any>{
       const userId = localStorage.getItem(USER_ID_KEY);
-      return this.http.get(`http://localhost:8080/api/users/logged/${userId}`);
+      return this.http.get<Prescription[]>(`http://localhost:8080/prescriptions/forPatient/${userId}`);
     }
   }
