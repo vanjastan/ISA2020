b988fc2b7613f492559d4c33cdc1ad6973aabcb9
Jelena <stojanovicj97@gmail.com>
2021-02-04 18:02:25 +0100

    User and examination controller tests

---
+++ Pharmacy/src/test/java/com/example/Pharmacy/constants/ExaminationConstants.java
@@ -0,0 +1,21 @@
+package com.example.Pharmacy.constants;
+
+public class ExaminationConstants {
+    public static final String NEW_DATE_EXAMINATION = "02.11.2020.";
+    public static final String NEW_DURATION = "1.5h";
+    public static final String NEW_PRICE = "1000";
+    public static final String NEW_RATE = "Excellent";
+    public static final String NEW_TIME_EXAM = "10:30";
+    public static final Long NEW_ID = 4L;
+
+    public static final String DB_DATE_EXAMINATION = "11.11.2020.";
+    public static final String DB_DURATION = "1h";
+    public static final String DB_PRICE = "2000";
+    public static final String DB_RATE = "Excellent";
+    public static final String DB_TIME_EXAM = "11:03";
+    public static final Long DB_ID = 1L;
+
+    public static final Long DB_PATIENT_ID = 3L;
+
+    public static final Long DB_REFERENCED_ID = 3L;
+}
--- Pharmacy/src/test/java/com/example/Pharmacy/controller/UserControllerTest.java
+++ Pharmacy/src/test/java/com/example/Pharmacy/controller/UserControllerTest.java
@@ -1,17 +1,51 @@
 package com.example.Pharmacy.controller;
 
+import com.example.Pharmacy.model.User;
 import com.example.Pharmacy.service.UserService;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.nio.charset.Charset;
 
 public class UserControllerTest extends ApplicationTests {
 
+    private static final String URL_PREFIX = "/api/users";
+
     private MockMvc mvc;
 
+    @MockBean
     private UserService userService;
 
-    @Test
-    void editUser() throws Exception{
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
 
+    @Test
+    @Transactional
+    @Rollback(true)
+    public void testEditPatient() throws Exception {
+        User patient = new User();
+        patient.setName("Maja");
+		patient.setSurname("Markovic");
+		patient.setUsername("maki");
+        patient.setCity("Beograd");
+        patient.setCountry("Srbija");
+        patient.setAddress("Markovica Marka 10");
+        patient.setNumber("065224566");
+
+        userService.save(patient);
+    }
+
+    @Test
+    @Transactional
+    @Rollback(true)
+    public void testEditPatientsPassword() throws Exception {
+        User patient = new User();
+        patient.setPassword("$2y$12$I6Wg2298xZ.RW3mNLksrJe4Me3gv.MBl9.BM4tf.jx4JjqS43gKTe");
+
+        userService.save(patient);
     }
 }
