34716c68d2e83ddc3c2d7518563b5b9e55bc71a2
draganabogdanovic <dacaa9977@gmail.com>
2021-02-11 00:45:19 +0100

    Edit pricelist

--- Pharmacy/src/main/java/com/example/Pharmacy/controller/PricelistController.java
+++ Pharmacy/src/main/java/com/example/Pharmacy/controller/PricelistController.java
@@ -38,7 +38,6 @@ public class PricelistController {
        List<PricelistDTO> pricelistDTO = new ArrayList<>();
 
        for(Pricelist p : pricelists){
-          // pricelistDTO.add(new PricelistDTO(p));
            PricelistDTO pl = new PricelistDTO();
            pl.setId(p.getId());
            pl.setPrice(p.getPrice());
@@ -53,7 +52,7 @@ public class PricelistController {
     }
 
     @GetMapping(value = "/{pharmacy_id}")
-   // @PreAuthorize("hasRole('ROLE_ADMINPH')")
+    //@PreAuthorize("hasRole('ROLE_ADMINPH')")
     public ResponseEntity<List<PricelistDTO>> getPLByPharmacyId(@PathVariable("pharmacy_id") Long pharmacy_id){
         List<Pricelist> pricelists = pricelistService.findAll();
 
